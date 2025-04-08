8f1c75c6392c66dda8eea3e45ef6751301833072
haritonenko <haritonenko04@mail.ru>
2025-04-08 20:40:06 +0700

    commit

--- src/main/java/org/example/quest/repository/QuestionAndAnswerRepository.java
+++ src/main/java/org/example/quest/repository/QuestionAndAnswerRepository.java
@@ -13,25 +13,25 @@ public class QuestionAndAnswerRepository {
     static {
         questions.put(1, new Question(1, "Ты потерял память. Принять вызов НЛО?", List.of(
                 new Answer(1, "Отклонить вызов", "Ты отклонил вызов.", 2),
-                new Answer(2, "Принять вызов", "Ты принял вызов.", 4)
+                new Answer(2, "Принять вызов", "Ты принял вызов.", 3)
         )));
 
         questions.put(2, new Question(2, "Поражение!", List.of()));
 
-        questions.put(4, new Question(4, "Поднимаешься на мостик к капитану?", List.of(
-                new Answer(4, "Отказаться", "Ты не пошел на переговоры.", 5),
-                new Answer(5, "Подняться", "Ты поднялся на мостик.", 6)
+        questions.put(3, new Question(3, "Поднимаешься на мостик к капитану?", List.of(
+                new Answer(3, "Отказаться", "Ты не пошел на переговоры.", 4),
+                new Answer(4, "Подняться", "Ты поднялся на мостик.", 5)
         )));
 
-        questions.put(5, new Question(5, "Поражение!", List.of()));
+        questions.put(4, new Question(4, "Поражение!", List.of()));
 
-        questions.put(6, new Question(6, "Ты кто?", List.of(
-                new Answer(6, "Солгать о себе", "Твоя ложь разоблачена.", 7),
-                new Answer(7, "Рассказать правду", "Тебя вернули домой.", 8)
+        questions.put(5, new Question(5, "Ты кто?", List.of(
+                new Answer(5, "Солгать о себе", "Твоя ложь разоблачена.", 6),
+                new Answer(6, "Рассказать правду", "Тебя вернули домой.", 7)
         )));
 
-        questions.put(7, new Question(7, "Поражение!", List.of()));
-        questions.put(8, new Question(8, "Победа!", List.of()));
+        questions.put(6, new Question(6, "Поражение!", List.of()));
+        questions.put(7, new Question(7, "Победа!", List.of()));
     }
 
     public Question getQuestionById(int id) {
